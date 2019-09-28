851eec294eb5033c33f28718e3f91b6f45175d7b
Pastor Cortes Osorno <pco07@hotmail.com>
2019-09-28 06:31:27 -0600

    Final details

--- DotNetComparison/Restaurant/App_Start/BundleConfig.cs
+++ DotNetComparison/Restaurant/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace Restaurant
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
